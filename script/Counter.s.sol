d73568169f41f7d5563037a4192a968683e15260
zerosnacks <zerosnacks@protonmail.com>
2024-04-08 22:43:18 +0200

    version bump

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.6.12;
+pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
--- src/AddLibrary.sol
+++ src/AddLibrary.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.6.12;
+pragma solidity ^0.8.13;
 
 library AddLibrary {
     function add(uint256 a, uint256 b) public pure returns (uint256) {
